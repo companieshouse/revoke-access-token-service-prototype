688c50be4b6a5b81559ebb4b62973c0072990671
Michael Gearon <mgearon@companieshouse.gov.uk>
2019-09-12 14:59:34 +0100

    Adding functionality

---
+++ app/data/data.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    id: 0,
+    app: 'Company App Name',
+    description: 'Optional description of the app',
+    terms: 'url',
+    privacy: 'url',
+    show: true,
+    read: true,
+    write: true,
+    approved: 'Monday, March 30, 2015 at 10:55:22 PM GMT+01:00'
+  }
+]
--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,47 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+// Route index page
+router.get('/', function (req, res) {
+  var realCases = require('./data/data.js')
+
+  req.session.cases = realCases.concat()
+  req.session.recents = []
+  req.session.notifications = {}
+  req.session.notifications.list = []
+  console.log(req.session.cases)
+  res.render('index')
+})
+router.all('*', function (req, res, next) {
+  if (typeof req.session.cases === 'undefined') {
+    return res.redirect('/')
+  }
+  next()
+})
+
+router.get('/access', function (req, res) {
+  console.log(req.session.cases)
+  console.log('1')
+
+  res.render('access', {
+    cases: req.session.cases
+  })
+})
+
+router.get('/revoke-access', function (req, res) {
+  var id = parseInt(req.query.id)
+  var i = 0
+
+  for (i = 0; i < req.session.cases.length; i++) {
+    if (req.session.cases[i].id === id) {
+      req.session.workingCase = req.session.cases[i]
+    }
+  };
+  console.log(req.session.workingCase)
+  res.render('revoke-access', {
+    case: req.session.cases[id]
+
+  })
+})
 
 module.exports = router
